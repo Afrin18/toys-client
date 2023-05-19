4999939552de741f15c5a28a16661499cf5dd34a
Afrin18 <gazifayzonnesaafrin18@gmail.com>
2023-05-19 14:03:43 +0600

    gallery

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import img1 from "../assets/cabbage-patch-kids.webp";
 import img2 from "../assets/collection-children-toys-kid-development-entertainment-isolated-white-background-bundle-tools-kid-amusement-play-bright-colored-vector-icons_87946-4696.avif";
-import img3 from "../assets/istockphoto-1182187147-612x612.jpg";
+import img3 from "../assets/toys-pictures_csp14363980.webp";
 import img4 from "../assets/Reviewed.com-RvEW-27182-octopus-plushie.webp";
 
 const Banner = () => {
@@ -10,8 +10,8 @@ const Banner = () => {
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
-                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
-                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                    <div className='space-y-7 w-3/5 pl-28'>
+                        <h2 className='text-7xl font-bold text-green-500'>"Play With Your Favorite Toys"</h2>
                         <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
                     </div>
                 </div>
@@ -23,14 +23,14 @@ const Banner = () => {
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img3} className="w-full" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
-                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
-                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                    <div className='space-y-7 w-3/5 pl-28'>
+                        <h2 className='text-7xl font-bold text-green-500'>"Play With Your Favorite Toys"</h2>
                         <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
                     </div>
                 </div>
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
-                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
-                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                    <div className='space-y-7 w-3/5 pl-28'>
+                        <h2 className='text-7xl font-bold text-green-500'>"Play With Your Favorite Toys"</h2>
                         <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
                     </div>
                 </div>
@@ -42,8 +42,8 @@ const Banner = () => {
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img2} className="w-full" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
-                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
-                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                    <div className='space-y-7 w-3/5 pl-28'>
+                        <h2 className='text-7xl font-bold text-green-500'>"Play With Your Favorite Toys"</h2>
                         <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
                     </div>
                 </div>
@@ -55,8 +55,8 @@ const Banner = () => {
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4}className="w-full" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
-                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
-                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                    <div className='space-y-7 w-3/5 pl-28'>
+                        <h2 className='text-7xl font-bold text-green-500'>"Play With Your Favorite Toys"</h2>
                         <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
                     </div>
                 </div>
