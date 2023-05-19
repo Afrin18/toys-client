6114013ccebf0f43b2096eb8d5f446054bbd1462
Afrin18 <gazifayzonnesaafrin18@gmail.com>
2023-05-19 07:56:07 +0600

    banner

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -6,35 +6,62 @@ import img4 from "../assets/Reviewed.com-RvEW-27182-octopus-plushie.webp";
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[700px] mt-4">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <h2>play with your favorite toys</h2>
+                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
+                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
+                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                        <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
+                    </div>
                 </div>
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-10">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img3} className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
+                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
+                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
+                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                        <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
+                    </div>
+                </div>
+                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
+                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
+                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                        <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-10">
+                    <a href="#slide1" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img2} className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
+                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
+                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
+                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                        <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-10">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4}className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
+                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0">
+                    <div className='text-white space-y-7 w-3/5 pl-28 text-green-500'>
+                        <h2 className='text-7xl font-bold'>"Play With Your Favorite Toys"</h2>
+                        <p className='text-3xl text-orange-500 font bold'>Welcome to the magical world of toys, where imagination knows no bounds and the possibilities for fun are endless!!!</p>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-10">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>
